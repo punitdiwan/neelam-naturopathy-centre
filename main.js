1489c34997c22ae624c645581c9634d61ca4cb87
Punit Diwan <punitdiwan@gmail.com>
2022-05-23 19:39:45 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// navbar
+$(function(){
+  var navbar = $('.navbar');
+  $(window).scroll(function(){
+    if($(window).scrollTop() <=40){
+      navbar.removeClass('navbar-scroll');
+    }
+    else{
+      navbar.addClass('navbar-scroll');
+    }
+  });
+});
+
+
